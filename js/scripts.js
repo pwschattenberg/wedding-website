0da1ca02652ff405b441b20937b97b57e494c2ad
Paul Schattenberg <pwschattenberg@gmail.com>
2024-04-19 22:20:43 +0200

    Update Google Sheets API

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'd390d3da2959fcf07b08b99257b0f310'
+            && MD5($('#invite_code').val()) !== 'd390d3da2959fcf07b08b99257b0f310') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbziLjX10Eyv-GbKwOMcWlIVrgV96SRwbiT6sa2XgXs62cl4heuWyH4-fBh-q3fkvnWT/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
